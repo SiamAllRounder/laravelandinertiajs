74f67056d7d43f33fee6004602fb4032a269fed0
siam <seum.bdit@gmail.com>
2023-11-12 18:32:36 +0600

    Use Inertia Link Component for Vue Js in Laravel Completed

--- resources/js/Components/MenuBar.vue
+++ resources/js/Components/MenuBar.vue
@@ -1,9 +1,15 @@
 <template>
     <h1>Salam from menu bar</h1>
+    <Link href="/">Home</Link> 
+    
+    <Link href="/product">Product</Link>
 </template>
 
-<style scoped>
+<script setup>
+import { Link } from '@inertiajs/vue3'
+</script>
 
+<style scoped>
 h1 {
     color: blue;
 }
